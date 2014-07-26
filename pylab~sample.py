b7145e977ffa407ed75e0eb598ed9ac955048a1b
enboyang <enboyang1990@gmail.com>
2014-07-26 14:32:59 +0800

    change pylab sample

--- pylab~sample.py
+++ pylab~sample.py
@@ -12,12 +12,28 @@ c,s=np.cos(x),np.sin(x)
 #define the figure indexes
 
 pl.figure(figsize=(8,6),dpi=80)
+#figure size with the unit
+#are inch
+
 pl.subplot(1,1,1)
+#this line is usually used for draw more
+#than one pictures
+
+#draw part
 pl.plot(x,c,color='blue',linewidth=1.0,linestyle='-')
 pl.plot(x,s,color='red',linewidth=2.0,linestyle='--')
-pl.xlim(-4,4)
-pl.ylim(-1,1)
+
+#setting the range of X & Y
+pl.xlim(x.min()*1.1,x.max()*1.1)
+pl.ylim(c.min()*1.1,c.max()*1.1)
+
+#settiong the ticks
 pl.xticks(np.linspace(-4,4,9,endpoint=True))
 pl.yticks(np.linspace(-1,1,5,endpoint=True))
-
+#or we can tell python which ticks shoule be displayed
+'''
+pl.xticks([-np.pi,-np.pi/2,0,np.pi/2,np.pi])
+pl.yticks([-1,0,1])
+#pay attention to the '[]',it's very important
+'''
 
